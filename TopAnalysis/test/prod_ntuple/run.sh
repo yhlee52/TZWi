9a6cd15f46b728347b3c68b5dde0af877abb6799
Junghwan John Goh <jhgoh@cern.ch>
2019-03-16 05:45:39 +0900

    correctly load modules, missing fix on header file

--- TopAnalysis/test/prod_ntuple/run.sh
+++ TopAnalysis/test/prod_ntuple/run.sh
@@ -25,10 +25,8 @@ DATATYPE2=`basename $FILELIST | sed -e 's;.txt;;g' | cut -d. -f2 | cut -d_ -f2`
 FILENAMES=$(cat $FILELIST | xargs -n$MAXFILES | sed -n "$(($JOBNUMBER+1)) p" | sed 's;/xrootd/;root://cms-xrdr.sdfarm.kr//xrd/;g')
 
 ARGS=""
-#ARGS="$ARGS -I PhysicsTools.NanoAODTools.python.postprocessing.modules.common.lepSFProducer"
-#ARGS="$ARGS -I PhysicsTools.NanoAODTools.python.postprocessing.modules.common.puWeightProducer"
-ARGS="$ARGS -I PhysicsTools.NanoAODTools lepSFProducer"
-ARGS="$ARGS -I PhysicsTools.NanoAODTools puWeightProducer"
+ARGS="$ARGS -I PhysicsTools.NanoAODTools.postprocessing.modules.common.lepSFProducer lepSF"
+ARGS="$ARGS -I PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer puWeight"
 ARGS="$ARGS -I TZWi.TopAnalysis.ttbarDoubleLepton ttbar$CHANNEL"
 ARGS="$ARGS -I TZWi.TopAnalysis.ttbarDoubleLeptonHLT ttbarHLT_${CHANNEL}_${DATATYPE2}"
 ARGS="$ARGS -I TZWi.TopAnalysis.ttbarDoubleLeptonHLT flags_${DATATYPE1}"
